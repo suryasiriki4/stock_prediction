3781946290383ea6719778ea735b229f0d598d8c
nagarjunaKolloju <cs18b018@iittp.ac.in>
2021-12-23 22:58:16 +0530

    added volume and volatility features

--- feature_extraction/feature_generation.py
+++ feature_extraction/feature_generation.py
@@ -6,6 +6,7 @@ produce_charts = True
 if produce_charts:
     import matplotlib.pyplot as plt
 
+
 def return_features(df):
     df["return"] = df["close"] / df["close"].shift(1)
     df["close_to_open"] = df["close"] / df["open"]
@@ -14,10 +15,12 @@ def return_features(df):
     df = df.iloc[1:]
     return df
 
+
 def target_value(df):
     df["y"] = df["return"].shift(-1)
     df = df.iloc[:len(df)-1]
     return df
+
 
 def trend_features(df):
     df = macd(df)
@@ -25,15 +28,17 @@ def trend_features(df):
     df = parabolic_sar(df)
     return df
 
+
 def macd(df):
-    ema_12_day = df["close"].ewm(com = (12-1)/2).mean()
-    ema_26_day = df["close"].ewm(com = (26-1)/2).mean()
+    ema_12_day = df["close"].ewm(com=(12-1)/2).mean()
+    ema_26_day = df["close"].ewm(com=(26-1)/2).mean()
     df["macd_line"] = ema_12_day - ema_26_day
     df["macd_9_day"] = df["macd_line"].ewm(com=(9-1)/2).mean()
     df["macd_diff"] = df["macd_line"] - df["macd_9_day"]
     if produce_charts:
         gen_charts.chart_macd(df)
     return df
+
 
 def ma(df):
     df["ma_50_day"] = df["close"].rolling(50).mean()
@@ -42,6 +47,7 @@ def ma(df):
     if produce_charts:
         gen_charts.chart_ma(df)
     return df
+
 
 def parabolic_sar(df):
     df["sar"] = np.nan
@@ -52,7 +58,8 @@ def parabolic_sar(df):
     extreme_point = min(df.loc[1:step, "close"])
     for i, row in df.iloc[step:].iterrows():
         if uptrend:
-            df.at[i, "sar"] = prior_sar + acc_factor * (extreme_point-prior_sar)
+            df.at[i, "sar"] = prior_sar + \
+                acc_factor * (extreme_point-prior_sar)
             if df.at[i, "low"] < df.at[i, "sar"]:
                 uptrend = False
                 prior_sar = max(df.loc[i-step:i, "close"])
@@ -62,7 +69,8 @@ def parabolic_sar(df):
                     extreme_point = df.at[i, "close"]
                     acc_factor = min(0.2, acc_factor + 0.02)
         else:
-            df.at[i, "sar"] = prior_sar - acc_factor * (prior_sar-extreme_point)
+            df.at[i, "sar"] = prior_sar - \
+                acc_factor * (prior_sar-extreme_point)
             if df.at[i, "high"] > df.at[i, "sar"]:
                 uptrend = True
                 prior_sar = min(df.loc[i-step:i, "close"])
@@ -83,6 +91,7 @@ def momentum_features(df):
     df = rsi(df)
     return df
 
+
 def stochastic_oscillator(df):
     lookback = 14
     df["stochastic_oscillator"] = ((df["close"] - df["close"].rolling(lookback).min()) /
@@ -90,6 +99,7 @@ def stochastic_oscillator(df):
     if produce_charts:
         gen_charts.chart_stochastic_oscillator(df)
     return df
+
 
 def commodity_channel_index(df):
     typical_price = (df["high"] + df["low"] + df["close"]) / 3
@@ -100,6 +110,7 @@ def commodity_channel_index(df):
     if produce_charts:
         gen_charts.chart_commodity_channel_index(df)
     return df
+
 
 def rsi(df):
     df["dollar_pnl"] = df["close"].shift(1) - df["close"]
@@ -120,3 +131,63 @@ def rsi(df):
         gen_charts.chart_rsi(df)
     return df
 
+
+def volatility_features(df):
+    df["5d_volatility"] = df["return"].rolling(5).std()
+    df["21d_volatility"] = df["return"].rolling(21).std()
+    df["60d_volatility"] = df["return"].rolling(60).std()
+    df = bollinger_bands(df)
+    df = average_true_range(df)
+    return df
+
+
+def bollinger_bands(df):
+    df["bollinger"] = ((df["close"] - df["close"].rolling(21).mean()) /
+                       2 * df["close"].rolling(21).std())
+    if produce_charts:
+        gen_charts.chart_bollinger(df)
+    return df
+
+
+def average_true_range(df):
+    high_vs_low = df["high"] - df["low"]
+    high_vs_prev_close = df["high"] - df["close"].shift(-1)
+    low_vs_prev_close = df["low"] - df["close"].shift(-1)
+    tr = high_vs_low.to_frame("high_vs_low")
+    tr["high_vs_prev_close"] = high_vs_prev_close
+    tr["low_vs_prev_close"] = low_vs_prev_close
+    tr["tr"] = tr.max(axis=1)
+    df["atr"] = tr["tr"].rolling(14).mean()
+    if produce_charts:
+        gen_charts.chart_average_true_range(df)
+    return df
+
+
+def volume_features(df):
+    df["volume_rolling"] = df["volume"] / df["volume"].shift(21)
+    df = on_balance_volume(df)
+    df = chaikin_oscillator(df)
+    return df
+
+
+def on_balance_volume(df):
+    df["dollar_pnl"] = df["close"].shift(1) - df["close"]
+    df["on_balance_volume"] = df["volume"]
+    df["on_balance_volume"] = df.apply(
+        lambda row: row.volume * -1 if row.dollar_pnl < 0 else row.volume, axis=1)
+    df["on_balance_volume"] = df["on_balance_volume"].cumsum()
+    if produce_charts:
+        gen_charts.chart_on_balance_volume(df)
+    return df
+
+
+def chaikin_oscillator(df):
+    money_flow_multiplier = (
+        (df["close"] - df["low"]) / (df["high"] - df["low"])) / (df["high"] - df["low"])
+    money_flow_volume = df["volume"] * money_flow_multiplier
+    adl = money_flow_volume.cumsum()
+    df["chaikin_oscillator"] = adl.ewm(
+        com=(3-1)/2).mean() - adl.ewm(com=(10-1)/2).mean()
+    if produce_charts:
+        gen_charts.chart_chaikin_oscillator(df)
+    return df
