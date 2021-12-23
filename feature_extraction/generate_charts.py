79ecee9b2012dd1ced07a80ef867dcceb2c29a99
nagarjunaKolloju <cs18b018@iittp.ac.in>
2021-12-23 23:03:04 +0530

    adding chart generation code for the features made

--- feature_extraction/generate_charts.py
+++ feature_extraction/generate_charts.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 import warnings
 
+
 def chart_macd(df):
     fig, axes = plt.subplots(2, 1, figsize=(10, 10))
     fig.tight_layout()
@@ -16,6 +17,7 @@ def chart_macd(df):
     df.tail(300)[["date", "macd_line", "macd_9_day"]].plot(
         x="date", kind="line", ax=ax2, secondary_y=False)
     fig.savefig("feature_charts/macd.png")
+
 
 def chart_ma(df):
     fig, axes = plt.subplots(1, 1, figsize=(10, 10))
@@ -33,6 +35,7 @@ def chart_ma(df):
         x="date", kind="line", ax=axes, secondary_y=True)
     fig.savefig("feature_charts/ma.png")
 
+
 def chart_sar(df):
     fig, axes = plt.subplots(1, 1, figsize=(10, 10))
     fig.tight_layout()
@@ -45,6 +48,7 @@ def chart_sar(df):
         x="date", style=".", ax=axes, secondary_y=False)
     fig.savefig("feature_charts/sar.png")
 
+
 def chart_stochastic_oscillator(df):
     fig, axes = plt.subplots(1, 1, figsize=(10, 10))
     fig.tight_layout()
@@ -54,7 +58,8 @@ def chart_stochastic_oscillator(df):
         x="date", kind="line", ax=axes, secondary_y=False)
     df.tail(100)[["date", "stochastic_oscillator"]].plot(
         x="date", kind="line", ax=axes, secondary_y=True)
-    fig.savefig("feature_charts/stochastic_oscillator.png")        
+    fig.savefig("feature_charts/stochastic_oscillator.png")
+
 
 def chart_commodity_channel_index(df):
     fig, axes = plt.subplots(1, 1, figsize=(10, 10))
@@ -67,6 +72,7 @@ def chart_commodity_channel_index(df):
         x="date", kind="line", ax=axes, secondary_y=True)
     fig.savefig("feature_charts/commodity_channel_index.png")
 
+
 def chart_rsi(df):
     fig, axes = plt.subplots(1, 1, figsize=(10, 10))
     fig.tight_layout()
@@ -78,3 +84,50 @@ def chart_rsi(df):
         x="date", kind="line", ax=axes, secondary_y=True)
     fig.savefig("feature_charts/rsi.png")
 
+
+def chart_bollinger(df):
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("GOOG Bollinger Bands", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace=0.4)
+    df.tail(100)[["date", "close"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "bollinger"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("feature_charts/bollinger.png")
+
+
+def chart_average_true_range(df):
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("GOOG Average True Range (ATR)", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace=0.4)
+    df.tail(100)[["date", "close"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "atr"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("feature_charts/average_true_range.png")
+
+
+def chart_on_balance_volume(df):
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("GOOG On Balance Volume (OBV)", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace=0.4)
+    df.tail(100)[["date", "close"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "on_balance_volume"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("feature_charts/on_balance_volume.png")
+
+
+def chart_chaikin_oscillator(df):
+    fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+    fig.tight_layout()
+    plt.suptitle("GOOG Chaikin Oscillator", fontsize=24)
+    plt.subplots_adjust(left=0.1, top=0.9, right=0.9, hspace=0.4)
+    df.tail(100)[["date", "close"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=False)
+    df.tail(100)[["date", "chaikin_oscillator"]].plot(
+        x="date", kind="line", ax=axes, secondary_y=True)
+    fig.savefig("feature_charts/chaikin_oscillator.png")
